cd79fcaa8cf3e4199cc15894eae04270842fa647
arthferreirw <arthur.pbf56@gmail.com>
2026-08-23 11:07:34 -0300

    fix: set base path in vite.config.ts for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/rota-15-hackaton-prototipo/',
   plugins: [react(), tailwindcss()],
 })
 
